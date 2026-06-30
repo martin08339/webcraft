25725b13b969e7968bb8d34737f3d1dde0a151b9
martin08339 <martin.080606n@gmail.com>
2026-06-30 10:07:00 -0600

    chore: Actualizar credenciales de Firebase

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,12 +4,13 @@
 
 // ⚠️ REEMPLAZA ESTOS VALORES CON LOS DE TU PROYECTO DE FIREBASE ⚠️
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "tu-proyecto.firebaseapp.com",
-  projectId: "tu-proyecto",
-  storageBucket: "tu-proyecto.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyC8gaARvqbfKPLURFBFG09V_GQTc6kyWvM",
+  authDomain: "webcraft-85468.firebaseapp.com",
+  projectId: "webcraft-85468",
+  storageBucket: "webcraft-85468.firebasestorage.app",
+  messagingSenderId: "794454105787",
+  appId: "1:794454105787:web:ab2d0b8e7d2d870e2f15b6",
+  measurementId: "G-HMPLEHTS1R"
 };
 
 // Inicializar Firebase (solo si no se ha inicializado antes)
